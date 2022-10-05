b2ca5369e3f77c1328b2de93a3d7ff372041815a
EmmanuelNBilkis <emmanuelbilkis@gmail.com>
2022-10-05 19:52:33 -0300

    punto 1 y 2 ok

---
+++ LogTest/Program.cs
@@ -0,0 +1,19 @@
+﻿public static class Program
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public static void Main()
+    {
+        try
+        {
+           Logger.Info("Hello world");
+           Logger.Info("Hello {Name}", "Earth");
+           Logger.Debug("Holaa");
+           System.Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+           Logger.Error(ex, "Goodbye cruel world");
+        }
+    }
+}  
